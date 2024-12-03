faa0451751145930781ed1a5e75cad46360ce894
Jean <jeanjyjh@gmail.com>
2024-12-03 17:25:33 +1300

    feat: Implement drag and drop board item management
    - Handle same board movement: Update single board array
    - Handle cross board movement: Update both source and destination boards

--- src/components/DragDrop.tsx
+++ src/components/DragDrop.tsx
@@ -1,13 +1,8 @@
-import {
-  DragDropContext,
-  Droppable,
-  Draggable,
-  DropResult,
-} from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
-import { toDosState } from "../atoms";
+import { toDoState } from "../atoms";
+import Board from "./Board";
 import styled from "styled-components";
-import Card from "./Card";
 
 const Wrapper = styled.div`
   display: flex;
@@ -21,45 +16,53 @@ const Wrapper = styled.div`
 const Boards = styled.div`
   display: grid;
   grid-gap: 10px;
-  width: 300px;
-  grid-template-columns: repeat(1, 1fr);
-`;
-
-const Board = styled.div`
-  padding: 30px 10px;
-  padding-bottom: 10px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 150px;
+  width: 100%;
+  max-width: 980px;
+  grid-template-columns: repeat(3, 1fr);
 `;
 
 function DragDrop() {
-  const [toDos, setToDos] = useRecoilState(toDosState);
+  //toDos = 기존의 toDos{모든보드}상태 { "TODO": ["task1", "task2"], "DOING": ["task3"], "DONE": ["task4"] }
+  const [toDos, setToDos] = useRecoilState(toDoState);
 
   const handleDragEnd = ({ destination, source }: DropResult) => {
     if (!destination) return;
-    const newToDos = Array.from(toDos);
-    const [reorderedItem] = newToDos.splice(source.index, 1);
-    newToDos.splice(destination.index, 0, reorderedItem);
-    setToDos(newToDos);
-    console.log("destination:", destination, "source:", source);
+
+    // Same board Move
+    if (destination.droppableId === source.droppableId) {
+      const boardCopy = [...toDos[source.droppableId]];
+      const draggedTodo = boardCopy[source.index];
+      boardCopy.splice(source.index, 1);
+      boardCopy.splice(destination.index, 0, draggedTodo);
+      setToDos({
+        ...toDos,
+        [source.droppableId]: boardCopy, // = { "TODO": ["b", "c", "a"] }
+      });
+    }
+
+    // Cross board Move
+    if (destination.droppableId !== source.droppableId) {
+      const sourceBoard = [...toDos[source.droppableId]];
+      const draggedTodo = sourceBoard[source.index];
+      const destinationBoard = [...toDos[destination.droppableId]];
+      sourceBoard.splice(source.index, 1);
+      destinationBoard.splice(destination.index, 0, draggedTodo);
+      setToDos({
+        ...toDos,
+        [source.droppableId]: sourceBoard,
+        [destination.droppableId]: destinationBoard,
+      });
+      console.log(source, destination);
+    }
   };
+
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
       <Wrapper>
         <Boards>
-          <Droppable droppableId="one">
-            {(provided) => (
-              <Board ref={provided.innerRef} {...provided.droppableProps}>
-                {toDos.map((toDo, index) => (
-                  <Draggable key={toDo} draggableId={toDo} index={index}>
-                    {(provided) => <Card todo={toDo} provided={provided} />}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </Board>
-            )}
-          </Droppable>
+          {Object.keys(toDos).map((boardId) => (
+            <Board key={boardId} toDos={toDos[boardId]} boardId={boardId} />
+          ))}
         </Boards>
       </Wrapper>
     </DragDropContext>
